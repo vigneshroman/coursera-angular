5f610550b0f172af25caab3065e0acd4c2f6b0e6
vigneshroman <vikizippo@gmail.com>
2021-08-18 10:46:09 +0530

    Components Part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
-import 'hammerjs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
