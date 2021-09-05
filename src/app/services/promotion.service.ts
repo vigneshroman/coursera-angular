7df88f3e939c256e1a27e1f6c0a62d6f7a6bf24e
vigneshroman <vikizippo@gmail.com>
2021-09-05 18:44:56 +0530

    Promises Part1

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -8,16 +8,16 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions():Promotion[]{
-    return PROMOTIONS
+  getPromotions():Promise<Promotion[]>{
+    return Promise.resolve(PROMOTIONS)
   }
 
-  getPromotion(id:String):Promotion{
-    return PROMOTIONS.filter((promotion) => promotion.id===id)[0];
+  getPromotion(id:String):Promise<Promotion>{
+    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.id===id)[0]);
   }
 
-  getFeaturedPromotion():Promotion{
-    return PROMOTIONS.filter((promotion) => promotion.featured)[0];
+  getFeaturedPromotion():Promise<Promotion>{
+    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]);
 
   }
 }
