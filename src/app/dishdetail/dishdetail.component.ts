7a2e384a1831fc1c537d3b3b8fa38d8f397b3720
vigneshroman <vikizippo@gmail.com>
2021-09-11 11:30:42 +0530

    RxJs Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,6 +5,8 @@ import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import {Dish} from '../shared/dish'
 
+import { switchMap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -17,15 +19,29 @@ export class DishdetailComponent implements OnInit {
 // @Input() 
 
   dish:Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor( private route:ActivatedRoute,
     private dishService:DishService, 
     private location:Location) { }
 
+  // ngOnInit() {
+  //   let id=this.route.snapshot.params['id'];
+  //   this.dishService.getDish(id)
+  //   .subscribe(dish=> this.dish=dish)
+  // }
   ngOnInit() {
-    let id=this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-    .subscribe(dish=> this.dish=dish)
+    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack():void{
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -18,14 +18,16 @@ export class DishService {
     }
      
    getDish(id:String):Observable<Dish>{
-    return of( DISHES.filter(dish => dish.id === id)[0] ).pipe(delay(2000));
+    return of( DISHES.filter(dish => dish.id === id)[0] ).pipe(delay(1000));
     
   }
 
   getFeaturedDish():Observable<Dish>{
     return of( DISHES.filter(dish => dish.featured)[0] ).pipe(delay(2000));
 }
-  
+getDishIds():Observable<string[] | any>{
+  return of(DISHES.map(dish=>dish.id));
+}
 
   // getDishes():Promise<Dish[]>{
   //   return new Promise(resolve=>{
